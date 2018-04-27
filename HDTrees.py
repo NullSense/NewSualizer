cbaf3e7a3e7e11a70b9a2501c272bb294d9975f9
Matas Pečiukonis <matas234@gmail.com>
2018-04-28 00:39:35 +0200

    Fixed parsing of ncbi taxonomy, parsing and tree printing

--- HDTrees.py
+++ HDTrees.py
@@ -1,7 +1,38 @@
 from ete3 import Tree
 
-file = "Trees/ancestor.tre"
 
-t = Tree(file)
+# scan file and output as string
+def scan_file(file):
+    with open(file) as f:
+        lines = f.readlines()
 
-print(t)
+    lines_string = ''.join(lines)
+    return lines_string
+
+
+# Remove leading characters before '(' to correctly parse tree
+def remove_leading(string):
+    string = '(' + string.split('(', 1)[1]
+    return string
+
+
+def write_to_file(string, file):
+    with open(file, 'w') as text_file:
+        text_file.write(string)
+
+
+def main():
+    file = "Trees/ncbi-taxonomy.tre"
+
+    tree_string = scan_file(file)
+    tree_string = remove_leading(tree_string)
+
+    print(tree_string[0:19])
+
+    # temporary workaround, cause Tree() thinks string is path
+    write_to_file(tree_string, file)
+    tree = Tree(file, format=1)
+    print(tree)
+
+
+main()
