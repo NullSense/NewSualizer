f4f62763f121c36290ccdeb45f1da4b0fd37cf4a
putnokiabel <putnokiabel@gmail.com>
2018-05-01 20:19:34 +0200

    -

--- viz.py
+++ viz.py
@@ -7,46 +7,47 @@ y = []
 s = []
 colors = []
 
+
 def main(tree):
-	global x,y,s
-	computeNode(tree,0.0,0.0,50.0,1,0.0)
-	# output to static HTML file
-	output_file("line.html")
+    global x, y, s
+    computeNode(tree, 0.0, 0.0, 50.0, 1, 0.0)
+    # output to static HTML file
+    output_file("line.html")
 
-	p = figure(plot_width=400, plot_height=400, x_range=(-100,100),y_range=(-100,100))
+    p = figure(plot_width=400, plot_height=400,
+               x_range=(-100, 100), y_range=(-100, 100))
 
-	# add a circle renderer with a size, color, and alpha
-	p.circle(x, y, radius=s, color=colors, alpha=1)
+    # add a circle renderer with a size, color, and alpha
+    p.circle(x, y, radius=s, color=colors, alpha=1)
 
-	# show the results
-	show(p)
-
-def computeNode(node,xx,yy, ss, depth,ang):
-	global x,y,s,colors
-	#x=[1, 2, 3, 4, 5]
-	#y=[6, 7, 2, 4, 5]
-	#s=[10,20,30,40,50]
-
-	t=2*math.pi if depth==1 else math.pi/1.3
-
-	x.append(xx)
-	y.append(yy)
-	s.append(ss)
-
-	colors.append("#{:02x}{:02x}{:02x}".format(int(depth*50),int(depth*50),150))
-
-	m=len(node.children)
-	if(m==0):
-		return
-
-	for i in range(m):
-		angle=(ang-t/2+t*i/m+t/(2*m))
-		rad = min(ss*math.sin(t/(2*m)),ss/2)
-		computeNode(node.children[i],	\
-			xx+math.cos(angle)*ss,		\
-			yy+math.sin(angle)*ss,		\
-			rad,						\
-			depth+1,
-			angle)
+    # show the results
+    show(p)
 
 
+def computeNode(node, xx, yy, ss, depth, ang):
+    global x, y, s, colors
+
+    t = 2 * math.pi if depth == 1 else math.pi / 1.3
+
+    x.append(xx)
+    y.append(yy)
+    s.append(ss)
+
+    colors.append("#{:02x}{:02x}{:02x}".format(
+        int(depth * 50), 
+        int(depth * 50), 
+        150))
+
+    m = len(node.children)
+    if(m == 0):
+        return
+
+    for i in range(m):
+        angle = (ang - t / 2 + t * i / m + t / (2 * m))
+        rad = min(ss * math.sin(t / (2 * m)), ss / 2)
+        computeNode(node.children[i],
+                    xx + math.cos(angle) * ss,
+                    yy + math.sin(angle) * ss,
+                    rad,
+                    depth + 1,
+                    angle)
