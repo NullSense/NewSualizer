058bf2e0757e2e2e5972f6f98cec6001d699a777
Matas Pečiukonis <matas234@gmail.com>
2018-05-08 17:55:25 +0200

    Added number of elements display, cleaned up code

--- viz.py
+++ viz.py
@@ -3,31 +3,50 @@ from bokeh.models import *
 from bokeh.layouts import gridplot
 import math
 
-# These store the layout of the first visualisation
-x1 = []
-y1 = []
-s1 = []  # size of circles
-colors1 = []
-name = []
-n_leaves = []
-
-# These store the layout of the second visualisation
-x2 = []
-y2 = []
-s2 = []  # size of circles
-colors2 = []
+num_elements = 0
 
 
 def main(tree):
+    fig_list = compute_figures(tree)
+
+    pre = PreText(text="Number of elements: " + str(num_elements))
+
+    show(gridplot([[pre], fig_list]))
+
+
+def compute_figures(tree):
+    # These store the layout of the first visualisation
+    x1 = []
+    y1 = []
+    radius1 = []  # size of circles
+    colors1 = []
+    name = []
+    num_leaves = []
+    num_elements = 0  # number of elements in the whole tree
     # Compute the layout of both visualisations
-    computeNode1(tree, 0.0, 0.0, 50.0, 1, 0.0)
-    computeNode2(tree, 0.0, 0.0, 80.0, 1)
+    num_elements = compute_visualization1(tree, x1, y1, radius1, colors1, name,
+                                          num_leaves, 0.0, 0.0, 50.0, 1, 0.0)
+
+    # These store the layout of the second visualisation
+    x2 = []
+    y2 = []
+    radius2 = []  # size of circles
+    compute_visualization2(tree, x2, y2, radius2, 0.0, 0.0, 80.0, 1)
 
     # output to static HTML file
     output_file("line.html")
 
     source = ColumnDataSource(
-        data=dict(x1=x1, y1=y1, s1=s1, x2=x2, y2=y2, s2=s2, colors1=colors1, name=name, n_leaves=n_leaves))
+        data=dict(
+            x1=x1,
+            y1=y1,
+            radius1=radius1,
+            x2=x2,
+            y2=y2,
+            radius2=radius2,
+            colors1=colors1,
+            name=name,
+            num_leaves=num_leaves))
 
     # tools for each fig
     tools = [
@@ -39,7 +58,8 @@ def main(tree):
         ResetTool(),
         PanTool(),
         TapTool(),
-        HoverTool(tooltips=[("Name: ", "@name"), ("Radius: ", "@s2"), ("Leaves in subtree: ", "@n_leaves")])
+        HoverTool(tooltips=[("Name: ", "@name"), ("Leaves in subtree: ",
+                                                  "@num_leaves")])
     ]
 
     # dimensions and tools of each fig
@@ -56,7 +76,7 @@ def main(tree):
     fig_list[0].circle(
         'x1',
         'y1',
-        radius='s1',
+        radius='radius1',
         fill_color='colors1',
         line_color='colors1',
         alpha=1,
@@ -65,24 +85,28 @@ def main(tree):
     fig_list[1].circle(
         'x2',
         'y2',
-        radius='s2',
+        radius='radius2',
         fill_color='red',
         line_color='red',
         alpha=0.1,
         source=source)
 
-    show(gridplot([fig_list]))
+    return fig_list
 
 
 # Computes the layout of the first visualisation
-def computeNode1(node, xx, yy, size, depth, ang):
+def compute_visualization1(node, x1, y1, radius1, colors1, name, num_leaves,
+                           xx, yy, size, depth, ang):
+    global num_elements
+    num_elements += 1
+
     t = 2 * math.pi if depth == 1 else math.pi / 1.3
 
     x1.append(xx)
     y1.append(yy)
-    s1.append(size)
+    radius1.append(size)
     name.append('unnamed' if node.name == '' else node.name)
-    n_leaves.append(len(node))
+    num_leaves.append(len(node))
 
     colors1.append("#{:02x}{:02x}{:02x}".format(
         int(depth * 50), int(depth * 50), 150))
@@ -95,15 +119,18 @@ def computeNode1(node, xx, yy, size, depth, ang):
         angle = (ang - t / 2 + t * i / m + t / (2 * m))
 
         radius = min(size * math.sin(t / (2 * m)), size / 2)
-        computeNode1(node.children[i], xx + math.cos(angle) * size,
-                     yy + math.sin(angle) * size, radius, depth + 1, angle)
+        compute_visualization1(node.children[i], x1, y1, radius1, colors1,
+                               name, num_leaves, xx + math.cos(angle) * size,
+                               yy + math.sin(angle) * size, radius, depth + 1,
+                               angle)
+    return num_elements
 
 
 # Computes the layout of second visualisation
-def computeNode2(node, xx, yy, size, depth):
+def compute_visualization2(node, x2, y2, radius2, xx, yy, size, depth):
     x2.append(xx)
     y2.append(yy)
-    s2.append(size)
+    radius2.append(size)
 
     m = len(node.children)
     if (m == 0):
@@ -112,5 +139,7 @@ def computeNode2(node, xx, yy, size, depth):
     for i in range(m):
         angle = i * 2 * math.pi / m
         radius = (math.sin(math.pi / m) * size) / (1 + math.sin(math.pi / m))
-        computeNode2(node.children[i], xx + math.cos(angle) * (size - radius),
-                     yy + math.sin(angle) * (size - radius), radius, depth + 1)
+        compute_visualization2(node.children[i], x2, y2, radius2,
+                               xx + math.cos(angle) * (size - radius),
+                               yy + math.sin(angle) * (size - radius), radius,
+                               depth + 1)
